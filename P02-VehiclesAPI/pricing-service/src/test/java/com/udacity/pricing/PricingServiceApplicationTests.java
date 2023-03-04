b5fd652276eab82f39bb1b05afc89079931f3e2d
unknown <aabdenba@gmail.com>
2023-03-04 13:01:10 -0500

    final commit before submit to udacity

--- P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
+++ P02-VehiclesAPI/pricing-service/src/test/java/com/udacity/pricing/PricingServiceApplicationTests.java
@@ -1,16 +1,42 @@
 package com.udacity.pricing;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import com.udacity.pricing.api.PricingController;
+import com.udacity.pricing.service.PriceException;
+import com.udacity.pricing.service.PricingService;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.math.BigDecimal;
+
+import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class PricingServiceApplicationTests {
 
+    @Autowired
+	PricingController pricingController;
+
 	@Test
-	public void contextLoads() {
+	public void contextLoads()  {
 	}
+	@Test
+	public void testPriceNotFoundThrowException(){
+//		System.out.println(pricingController.get(100L));
+		ResponseStatusException thrown = assertThrows(
+				ResponseStatusException.class,
+				() -> pricingController.get(100L),
+				"Price Not Found"
+		);
+		System.out.println(thrown.getStatus().getClass());
+		assertTrue(thrown.getStatus().toString().equals("404 NOT_FOUND"));
+	}
+
 
 }
